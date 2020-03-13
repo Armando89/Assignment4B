4dfd804145e04286e255e24b9ed5edd20781b203
Armando89 <43739205+Armando89@users.noreply.github.com>
2020-03-12 21:11:19 -0500

    started case2

--- src/assignment4b/Assignment4B.java
+++ src/assignment4b/Assignment4B.java
@@ -19,15 +19,18 @@ public class Assignment4B
         //JOptionPane.showMessageDialog(null,"Welcome to the HR payroll program");
         
         //double ANN_BONUS = .05;
-        double annBonus;
+        //double annBonus;
         //double YR_SALARY = 50000;
-        double yrSal;
+        //double yrSal;
         
-        double HR_RATE = 20;
-        double BASE_PAY = 200;
-        double COMMISSION = .10;
-        double wPaycheck, otPay, regularPay, totalPay, totalAmount, comm, priceItem;
-        int answer, hrs, otHours, soldItems;
+        //double hrRate;
+        
+        //double BASE_PAY = 200;
+        //double COMMISSION = .10;
+        double yrSal, annBonus, wPaycheck, hrRate;
+        //otPay, regularPay, totalPay, totalAmount, comm, priceItem;
+        int answer, hrs, otHours; 
+        //soldItems;
         String fName, lName;
              
         do 
@@ -62,12 +65,12 @@ public class Assignment4B
                     lName = JOptionPane.showInputDialog("Please enter the employee's "
                             + "last name:");
                     yrSal = Double.parseDouble(JOptionPane.showInputDialog("Please "
-                            + "provide the employee's yearly salary: $"));
+                            + "provide the employee's yearly salary:"));
                         while (yrSal < 30000 || yrSal > 65000)
                         {
                             yrSal = Double.parseDouble(JOptionPane.showInputDialog
-                                ("Invalid input: The amount must be between $30000 "
-                                        + "and $65000."));
+                                ("Invalid input: The amount must be between $30,000 "
+                                        + "and $65,000."));
                         }
                         /*if (yrSal >= 30000 && yrSal <= 65000)
                         {
@@ -81,32 +84,54 @@ public class Assignment4B
                             continue;
                         }*/
                     annBonus = Double.parseDouble(JOptionPane.showInputDialog("Please "
-                            + "provide the annual profit bonus percent: %"));
+                            + "provide the annual profit bonus percent:"));
+                    
+                    wPaycheck = (yrSal * (1 + annBonus)) / 52;
                     
                     //String message = String.format("Welcome to the payroll calculator %s %s.  Your salary is %d and your bonus is %.2f", fName, lName, yearlySal, yearlyBonus);
-                    String message = String.format("This summary is for employee: %s %s. The employee's yearly salary is $%s not including the annual profit bonus of %.2f percent.", fName, lName, yrSal, annBonus);
+                    String message = String.format("This summary is for employee: "
+                            + "%s %s. The employee's weekly paycheck amount is "
+                            + "$%.2f and its yearly salary is $%.2f, which "
+                            + "doesn't include the annual profit bonus of %.2f "
+                            + "percent.", fName, lName, wPaycheck, yrSal, annBonus);
                     JOptionPane.showMessageDialog(null, message);
-                    break;       
-            }
-            /*while (answer == 1)
-            {
-                System.out.print("Please enter the employee's first name: ");
-                fName = input.next(); 
-                System.out.print("Please enter the employee's last name: ");
-                lName = input.next();
-                System.out.println("Please provide the employee's yearly salary: $" 
-                        + YR_SALARY); 
-                System.out.println("Please provide the annual profit bonus "
-                        + "percent: %" + ANN_BONUS);
-
-                wPaycheck = (YR_SALARY * (1 + ANN_BONUS)) / 52;
-
-                System.out.printf("%s%s%s%s%s%.2f%n%n", "The employee's name is ", 
-                        fName, " ", lName, " and its weekly paycheck amount is $", 
-                        wPaycheck);
-                break;
-            }*/
-                
+                    break;
+                    
+                case 2:
+                    fName = JOptionPane.showInputDialog("Please enter the employee's "
+                            + "first name:");
+                    lName = JOptionPane.showInputDialog("Please enter the employee's "
+                            + "last name:");
+                    hrRate = Double.parseDouble(JOptionPane.showInputDialog("Please "
+                            + "provide the employee's hourly rate:"));
+                        while (hrRate < 10 || hrRate > 25)
+                        {
+                            hrRate = Double.parseDouble(JOptionPane.showInputDialog
+                                ("Invalid input: The amount must be between $10.00 "
+                                    + "and $25.00."));
+                        }
+                    hrs = Integer.parseInt(JOptionPane.showInputDialog("Please "
+                            + "provide the total number of hours worked:"));
+                        while (hrs < 0)
+                        {
+                            hrs = Integer.parseInt(JOptionPane.showInputDialog
+                                ("Invalid input: The number of hours must be greater "
+                                    + "than 0."));
+                        }
+                         if (hrs > 40)
+                        {
+                            otHours = Integer.parseInt(JOptionPane.showInputDialog("Please "
+                                + "provide the number of overtime hours worked:"));
+                            //System.out.print("Please provide the number of overtime "
+                                //+ "hours worked: ");
+                            //otHours = input.nextInt();                        
+                            otPay = ((hrRate * 1.5) * otHours);
+                        }
+                        else
+                        {
+                            otPay = 0;
+                        }
+            }     
             /*while (answer == 2)
             {
                 System.out.print("Please enter the employee's first name: ");
